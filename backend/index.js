8443c2181b1554f8492964ac2fe5ee98e66189fd
aryanuu21 <aryanpratapsingh674@gmail.com>
2025-06-15 23:57:35 +0530

    changes made

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ let timeOnline = {}
 
 const io = new Server(server,{
     cors: {
-        origin: "*",
+        origin: "https://chehra.vercel.app",
         methods: ["GET", "POST"],
         allowedHeaders: ["*"],
         credentials: true
